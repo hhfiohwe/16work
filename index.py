a6ad2db104d70b275943cb48fb4dd86abd9cc369
游竣程 <daniel994033999@gmail.com>
2023-11-15 21:21:48 +0800

    work

--- index.py
+++ index.py
@@ -10,52 +10,15 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     homepage = "<h1>游竣程Python網頁</h1>"
-    homepage += "<a href=/mis>MIS</a><br>"
-    homepage += "<a href=/today>顯示日期時間</a><br>"
-    homepage += "<a href=/welcome?nick=游竣程>傳送使用者暱稱</a><br>"
-    homepage += "<a href=/about>游竣程簡介網頁</a><br>"
-    homepage += "<a href=/account>帳號密碼</a><br>"
-    homepage += "<br><a href=/read>人選之人演員</a><br>"
+    homepage += "<a href=/work>游竣程求職網頁</a><br>"
 
     return homepage
 
-@app.route("/mis")  
-def course():
-    return "<h1>資訊管理導論</h1>"
 
-@app.route("/today")
-def today():
-    now = datetime.now()
-    return render_template("today.html", datetime = str(now))
-
-@app.route("/welcome", methods=["GET", "POST"])
-def welcome():
-    user = request.values.get("nick")
-    return render_template("welcome.html", name=user)
-
-@app.route("/about")
+@app.route("/work")
 def about():
-    return render_template("about.html")
+    return render_template("16job.html")
 
-@app.route("/account", methods=["GET", "POST"])
-def account():
-    if request.method == "POST":
-        user = request.form["user"]
-        pwd = request.form["pwd"]
-        result = "您輸入的帳號是：" + user + "; 密碼為：" + pwd 
-        return result
-    else:
-        return render_template("account.html")
-
-@app.route("/read")
-def read():
-    Result = ""
-    db = firestore.client()
-    collection_ref = db.collection("人選之人")    
-    docs = collection_ref.get()    
-    for doc in docs:         
-        Result += "文件內容：{}".format(doc.to_dict()) + "<br>"    
-    return Result
 
 
 #if __name__ == "__main__":
